cf828121cf8227dd941b871575713889ee8e7044
evrhel <ethanvrhel@gmail.com>
2021-03-17 19:53:01 -0700

    Progress on implementing Object functions

--- lsasm/build.bat
+++ lsasm/build.bat
@@ -1,3 +1,3 @@
 
-..\x64\Release\lsasm.exe -d -o ..\lscript\lib -f Main.lasm -f String.lasm -f StringBuilder.lasm -f System.lasm -f Process.lasm -f StdFileHandle.lasm -f FileOutputStream.lasm -f FileInputStream.lasm -f Math.lasm -f Object.lasm
+..\x64\Release\lsasm.exe -d -o ..\lscript\lib -f Main.lasm -f String.lasm -f StringBuilder.lasm -f System.lasm -f Process.lasm -f StdFileHandle.lasm -f FileOutputStream.lasm -f FileInputStream.lasm -f Math.lasm -f Object.lasm -f Int.lasm -f Long.lasm
 pause
